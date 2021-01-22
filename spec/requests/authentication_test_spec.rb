cc5158b0461a211eed6039be772e91d739e15991
Br3-kyokyo <br3.kyokyo@gmail.com>
2021-01-23 04:31:38 +0900

    日本語にした

--- spec/requests/authentication_test_spec.rb
+++ spec/requests/authentication_test_spec.rb
@@ -6,49 +6,67 @@ include ActionController::RespondWith
 # The authentication header looks something like this:
 # {"access-token"=>"abcd1dMVlvW2BT67xIAS_A", "token-type"=>"Bearer", "client"=>"LSJEVZ7Pq6DX5LXvOWMq1w", "expiry"=>"1519086891", "uid"=>"darnell@konopelski.info"}
 
-describe 'Whether access is ocurring properly', type: :request do
+
+# 認証APIがうまく動いていることを確認するには
+# - アカウントを作るとcreatedが返ってくること
+# - アカウントを消すとdestroyedが返ってくること
+# - サインインするとトークンが得られること
+# - サインインすると200が返ってくること
+# - 受け取ったトークンで個人情報にアクセスできること
+# - 間違ったトークンでは個人情報にアクセスできないこと
+# - サインインしていない場合個人情報にアクセスできないこと
+
+
+
+
+describe '認証API: ', type: :request do
+  
   before(:each) do
-    @current_user = FactoryBot.create(:user)
+    @current_user = FactoryBot.create(:member)
     @client = FactoryBot.create(:client)
   end
 
-  context 'context: general authentication via API, ' do
-    it "doesn't give you anything if you don't log in" do
-      get api_client_path(@client)
-      expect(response.status).to eq(401)
+  context 'ログインしない場合' do 
+    it "401 Unauthorized エラーを返すこと" do
+        get client_path(@client)
+        expect(response.status).to eq(401)
+    end
+  end
+
+  context 'ログインする場合' do 
+
+    before do
+        login
     end
 
-    it 'gives you an authentication code if you are an existing user and you satisfy the password' do
-      login
-      # puts "#{response.headers.inspect}"
-      # puts "#{response.body.inspect}"
-      expect(response.has_header?('access-token')).to eq(true)
+    it '存在するユーザと正しいパスワードでアクセストークンを得られること' do
+        # puts "#{response.headers.inspect}"
+        # puts "#{response.body.inspect}"
+        expect(response.has_header?('access-token')).to eq(true)
     end
 
-    it 'gives you a status 200 on signing in ' do
-      login
-      expect(response.status).to eq(200)
+    it '200を返すこと' do
+        expect(response.status).to eq(200)
     end
 
-    it 'first get a token, then access a restricted page' do
-      login
-      auth_params = get_auth_params_from_login_response_headers(response)
-      new_client = FactoryBot.create(:client)
-      get api_find_client_by_name_path(new_client.name), headers: auth_params
-      expect(response).to have_http_status(:success)
+    it 'トークンを用いて、マイページ(restricted page)にアクセスできること' do
+        auth_params = get_auth_params_from_login_response_headers(response)
+        new_client = FactoryBot.create(:client)
+        get find_client_by_name_path(new_client.name), headers: auth_params
+        expect(response).to have_http_status(:success)
     end
 
-    it 'deny access to a restricted page with an incorrect token' do
-      login
-      auth_params = get_auth_params_from_login_response_headers(response).tap do |h|
-        h.each do |k, _v|
-          if k == 'access-token'
-            h[k] = '123'
-          end end
-      end
-      new_client = FactoryBot.create(:client)
-      get api_find_client_by_name_path(new_client.name), headers: auth_params
-      expect(response).not_to have_http_status(:success)
+    it '間違ったトークンを用いて、マイページにアクセスできないこと' do
+        auth_params = get_auth_params_from_login_response_headers(response).tap do |h|
+            h.each do |k, _v|
+                if k == 'access-token'
+                    h[k] = '123'
+                end 
+            end
+        end
+        new_client = FactoryBot.create(:client)
+        get find_client_by_name_path(new_client.name), headers: auth_params
+        expect(response).not_to have_http_status(:success)
     end
   end
 
@@ -63,12 +81,12 @@ describe 'Whether access is ocurring properly', type: :request do
       login
       auth_params = get_auth_params_from_login_response_headers(response)
       new_client = FactoryBot.create(:client)
-      get api_find_client_by_name_path(new_client.name), headers: auth_params
+      get find_client_by_name_path(new_client.name), headers: auth_params
       expect(response).to have_http_status(:success)
 
       allow(Time).to receive(:now).and_return(Time.now + token_age)
 
-      get api_find_client_by_name_path(new_client.name), headers: auth_params
+      get find_client_by_name_path(new_client.name), headers: auth_params
       response
     end
   end
@@ -79,7 +97,7 @@ describe 'Whether access is ocurring properly', type: :request do
   end
 
   def login
-    post api_user_session_path, params:  { email: @current_user.email, password: 'password' }.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    post user_session_path, params:  { email: @current_user.email, password: 'password' }.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
   end
 
   def get_auth_params_from_login_response_headers(response)
