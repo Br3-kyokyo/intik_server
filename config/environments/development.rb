e10f8355dff7137bf947b1dd46379c3d8891426f
Br3-kyokyo <br3.kyokyo@gmail.com>
2021-01-23 01:30:32 +0900

    development CORS setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.use Rack::Cors do
+    allow do
+      origins 'localhost:3000', 'localhost:'
+      resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        methods: [:get, :post, :options, :delete, :put]
+    end
+  end
 end
